b2c8992438f003c6835cdf51650bad41edea7615
kishore <kumarkishore02092002@gmail.com>
2024-04-26 18:37:09 +0530

    Change of function name Division to divide

--- calc.go
+++ calc.go
@@ -12,6 +12,7 @@ func Multiply(num1, num2 float64) float64 {
 	return num1 * num2;
 }
 
-func Division(num1, num2 float64) float64 {
+func Divide(num1, num2 float64) float64 {
 	return num1/num2;
 }
+
